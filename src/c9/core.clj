65fee1596abbbf059d0d562dc0b494f87cecc418
Akira Ota 10nin <akira19870221@gmail.com>
2018-09-05 19:53:39 +0900

    require jdbc and add in-memory databse setting.

--- src/c9/core.clj
+++ src/c9/core.clj
@@ -1,6 +1,11 @@
-(ns c9.core)
+(ns c9.core
+  (:require [clojure.java.jdbc :as j]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def db-settings
+  {
+   :classname "org.h2.Driver"
+   :subprotocol "h2:mem" ;; in-memory
+   :subname "c9inst;DB_CLOSE_DELAY=-1"
+   :user "sa"
+   :password ""
+   })
